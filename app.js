c67c21c0209cdc1b27cddd3d290f2fb386221fb2
Patrick Krueger <sendthistopatrick@gmail.com>
2022-08-05 11:50:48 -0600

    Create ability to play the game
    A round is started with the playRound() function. The player choice is
    tested against the computer choice and a message is returned based on
    the outcome.

--- app.js
+++ app.js
@@ -13,6 +13,41 @@ function getComputerChoice() {
 
 //Get input for player choice
 
-//Play a round
+//Play a round and display message
   //If player choice beats computer choice: win
   //Else: lose
+function playRound(playerChoice, computerChoice = getComputerChoice()) {
+  playerChoice = playerChoice.toLowerCase()
+
+  if (drawConditions(playerChoice, computerChoice)) {
+    return `Draw! You both picked ${firstCapital(playerChoice)}`
+
+  } else if (winConditions(playerChoice, computerChoice)) {
+    return `You win! ${firstCapital(playerChoice)} beats ` + 
+    firstCapital(computerChoice)
+
+  } else {
+    return `You lose! ${firstCapital(playerChoice)} is beaten by ` +
+    firstCapital(computerChoice)
+  }
+}
+
+//True if there is a draw
+function drawConditions(playerChoice, computerChoice) {
+  return playerChoice == computerChoice ? true : false
+}
+
+//True if player wins
+function winConditions(playerChoice, computerChoice) {
+  let pc = playerChoice
+  let cc = computerChoice
+  
+  return (pc == 'rock' && cc == 'scissors') || 
+  (pc == 'paper' && cc == 'rock') || 
+  (pc == 'scissors' && cc == 'paper')
+}
+
+//Capitalizes the first letter of the inputted string
+function firstCapital(string) {
+  return string[0].toUpperCase() + string.substring(1)
+}
